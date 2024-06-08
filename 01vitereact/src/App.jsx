b9ad3e4f11a5c08be9d581a395f1edc0ae2c96a3
Gopal <gdev3.js@gmail.com>
2024-06-08 08:53:37 +0530

    Hello World in React with vite

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <h1 className="G_dev3">hello World</h1>
+    </>
+  )
+}
+
+export default App
