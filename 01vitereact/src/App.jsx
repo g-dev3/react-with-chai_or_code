4b02d79e90415bf22ccbd8204fa83d5035e8f76b
Gopal <gdev3.js@gmail.com>
2024-06-09 07:21:28 +0530

    ceated new function in Vite with React for frangment

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,11 +1,9 @@
 import { useState } from 'react'
 
 function App() {
-  // const [count, setCount] = useState(0)
-
   return (
     <>
-    <h1 className="G_dev3">hello World</h1>
+    <chai />
     </>
   )
 }
---
+++ 01vitereact/src/Chai.jsx
@@ -0,0 +1,8 @@
+function Chai(){
+    return(
+        <h1>Chai is ready to Serve</h1>
+    )
+}
+
+
+export default Chai
